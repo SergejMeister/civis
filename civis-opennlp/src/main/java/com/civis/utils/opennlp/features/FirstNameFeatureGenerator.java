d007600263ed0b31b9417086aad90fcbe94567cc
SergejMeister <sergej_meister@gmx.net>
2015-08-27 01:51:56 +0200

    copyright apache license 2

--- civis-opennlp/src/main/java/com/civis/utils/opennlp/features/FirstNameFeatureGenerator.java
+++ civis-opennlp/src/main/java/com/civis/utils/opennlp/features/FirstNameFeatureGenerator.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2015 Sergej Meister
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.civis.utils.opennlp.features;
 
 import opennlp.tools.util.featuregen.FeatureGeneratorAdapter;
@@ -18,16 +34,6 @@ public class FirstNameFeatureGenerator extends FeatureGeneratorAdapter {
     private Set<String> names;
     private Set<Integer> indexes;
     private Set<String> foundedNames;
-
-    public FirstNameFeatureGenerator() {
-        this.names = new HashSet<>();
-        init();
-    }
-
-    public FirstNameFeatureGenerator(Set<String> names) {
-        this.names = names;
-        init();
-    }
 
     public FirstNameFeatureGenerator(Set<String> names, Set<String> excludeNames) {
         this.names = names;
